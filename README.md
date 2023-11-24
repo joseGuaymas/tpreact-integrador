d0279f6e4f2dbddb6abcfc8faf5bb8305eacc577
joseGuaymas <joseguaymas@hotmail.com>
2023-11-24 03:19:35 -0300

    Creamos los componentes TaskItem y TaskForm, y le dimos estilos básicos a la aplicación.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TP REACT UTN 
 
+Hola, arranqué creando dos componentes TaskItem y TaskForm.
+Les di estilos para tener una idea más abstracta pero 
+son re básicos y no pretenden ser definitivos.
+
+
+  
--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import TaskList from './componentes/TaskList'
+import { TaskItem } from './componentes/TaskItem';
+import TaskForm from './componentes/TaskForm'
 
 function App() {
   return (
     <div className="aplicacion-tareas">
       <div className='tareas-lista-principal'>
         <h1>Lista de Tareas</h1>
-       
+        <TaskForm />
+
+        <TaskItem />
       </div>
       
        
